6b5671c95b23cf25116a4e597013433fbb35a861
Austin Chun <achun@g.hmc.edu>
2017-07-22 17:25:36 -0700

    Added collision detection

--- Algorithm/gui.py
+++ Algorithm/gui.py
@@ -10,6 +10,9 @@ from tkinter import *
 import time
 import random
 
+firstCarIndex = 7
+overlaps = []
+
 class GUI():
 
     def __init__(self, rows, cols, width, height):
@@ -38,6 +41,10 @@ class GUI():
         # Create gridlines
         rowSpacing = height/(rows+1)
         colSpacing = width/(cols+1)
+        self.rowSpacing = rowSpacing
+        self.colSpacing = colSpacing
+
+
         for row in range(1,rows+1):
             self.w.create_line(0, row*rowSpacing, width, row*rowSpacing)
         for col in range(1,cols+1):
@@ -61,7 +68,7 @@ class GUI():
             self.cars.append(self.w.create_rectangle(
                                 0, (i+1)*rowSpacing+carWidth/2,
                                 carHeight, (i+1)*rowSpacing-carWidth/2,
-                                fill='red'))
+                                fill='blue'))
 
     
     def startCall(self):
@@ -77,37 +84,53 @@ class GUI():
 
 
     def move(self):
-        if self.notQuit:
-            if self.startFlag:
-                inc = 2
+        if self.startFlag:
+            inc = 2
+        else:
+            inc = 0
+
+        for col in range(self.cols):
+            if(self.w.coords(self.cars[col])[1] >= self.height):
+                self.w.move(self.cars[col], 0, -(self.height+self.carHeight))
+            # print(self.w.coords(self.cars[col])[3])
+            self.w.move(self.cars[col], 0, inc)
+        for row in range(self.rows):
+            if(self.w.coords(self.cars[self.cols+row])[0] >= self.width):
+                self.w.move(self.cars[self.cols+row], -(self.width+self.carHeight), 0)
+            self.w.move(self.cars[self.cols+row], inc, 0)
+        self.tk.update()
+
+    def checkCollision(self):
+        global firstCarIndex
+        global overlaps
+        for col in range(self.cols):
+            coords = self.w.coords(self.cars[col])
+            ov = self.w.find_overlapping(coords[0], coords[1], 
+                                               coords[2], coords[3])
+
+            ov = [car for car in ov if car >= firstCarIndex]
+            if(len(ov) > 1):
+                for car in ov:
+                    if car not in overlaps:
+                        overlaps += ov
+
+        for car in range(firstCarIndex, firstCarIndex+len(self.cars)):
+            if car in overlaps:
+                self.w.itemconfig(car, fill='red')
             else:
-                inc = 0
-
-            for col in range(self.cols):
-                if(self.w.coords(self.cars[col])[1] >= self.height):
-                    self.w.move(self.cars[col], 0, -(self.height+self.carHeight))
-                # print(self.w.coords(self.cars[col])[3])
-                self.w.move(self.cars[col], 0, inc)
-            for row in range(self.rows):
-                if(self.w.coords(self.cars[self.cols+row])[0] >= self.width):
-                    self.w.move(self.cars[self.cols+row], -(self.width+self.carHeight), 0)
-                self.w.move(self.cars[self.cols+row], inc, 0)
-            self.tk.update()
-
-
-
+                self.w.itemconfig(car, fill='blue')
+       
+        # Reset collision list
+        overlaps = []
 
 g = GUI(3,3,600,400)
 g.tk.update()
 
-# print(g.startFlag)
-# while not g.startFlag:
-    # print('.',end='')
-    
+
 while True:
-    # time.sleep(0.01)
     if g.notQuit:
         g.move()
+        g.checkCollision()
         time.sleep(0.01)
     else:
         g.tk.destroy()
